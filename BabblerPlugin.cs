28cbddd65e3d5608f141fca83f9bfbf83f2abe00
Arsonide <arsonide@yahoo.com>
2024-01-06 16:55:29 -0600

    Setting up all the boilerplate of setting up a new project.

---
+++ BabblerPlugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using SOD.Common.BepInEx;
+
+namespace Babbler;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class BabblerPlugin : PluginController<BabblerPlugin>
+{
+    public override void Load()
+    {
+        base.Load();
+        
+        Utilities.Log($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        
+        Harmony.PatchAll();
+        
+        Utilities.Log($"Plugin {MyPluginInfo.PLUGIN_GUID} is patched!");
+        
+        // ClassInjector.RegisterTypeInIl2Cpp<ClassName>();
+        // Utilities.Log($"Plugin {MyPluginInfo.PLUGIN_GUID} has added custom types!");
+    }
+}
---
+++ Helpers/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx.Logging;
+
+namespace Babbler;
+
+public static class Utilities
+{
+    public const bool DEBUG_BUILD = true;
+    
+    public static void Log(string message, LogLevel level = LogLevel.Info)
+    {
+        // Debug does not appear, presumably because it's for some functionality we don't have. We'll use it to filter based on DEBUG_BUILD instead.
+        if (level == LogLevel.Debug)
+        {
+#pragma warning disable CS0162
+
+            if (DEBUG_BUILD)
+            {
+                level = LogLevel.Info;
+            }
+            else
+            {
+                return;
+            }
+            
+#pragma warning restore CS0162
+        }
+        
+        BabblerPlugin.Log.Log(level, message);
+    }
+}
